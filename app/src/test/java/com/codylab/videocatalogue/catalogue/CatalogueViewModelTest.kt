cf2c4d6bee12c0904187fbbfeaf2092a4a8a44b8
Cody Liu <liu@codylab.com>
2018-12-09 08:18:15 +1100

    Add negative test case for CatalogueViewModel

--- app/src/test/java/com/codylab/videocatalogue/catalogue/CatalogueViewModelTest.kt
+++ app/src/test/java/com/codylab/videocatalogue/catalogue/CatalogueViewModelTest.kt
@@ -4,6 +4,7 @@ import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.codylab.videocatalogue.core.api.CatalogueRepository
 import com.codylab.videocatalogue.core.coroutine.DispatcherManager
 import com.codylab.videocatalogue.utils.TestDataUtils
+import com.codylab.videocatalogue.utils.TestObserver
 import com.codylab.videocatalogue.utils.testObserver
 import com.nhaarman.mockito_kotlin.whenever
 import kotlinx.coroutines.Dispatchers
@@ -29,9 +30,16 @@ class CatalogueViewModelTest {
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
 
+
+    private lateinit var viewModel: CatalogueViewModel
+
+    private lateinit var testObserver: TestObserver<CatalogueUIModel>
+
     @Before
     fun setUp() {
         whenever(dispatchManager.main).thenReturn(Dispatchers.Default)
+        viewModel = CatalogueViewModel(catalogueRepository, dispatchManager)
+        testObserver = viewModel.uiModelLiveData.testObserver()
     }
 
     @Test
@@ -39,8 +47,8 @@ class CatalogueViewModelTest {
         // Given
         val expectedCategories = TestDataUtils.getSampleCategories()
         val viewModel = CatalogueViewModel(catalogueRepository, dispatchManager)
-        whenever(catalogueRepository.getCategories()).thenReturn(expectedCategories)
         val testObserver = viewModel.uiModelLiveData.testObserver()
+        whenever(catalogueRepository.getCategories()).thenReturn(expectedCategories)
 
         // When
         async { viewModel.onLoad() }.await()
@@ -53,6 +61,28 @@ class CatalogueViewModelTest {
 
         val dataUiModel = observedValues[1]!!
         Assert.assertEquals(expectedCategories, dataUiModel.categories)
+
+        val onCompletedUiModel = observedValues[2]!!
+        Assert.assertFalse(onCompletedUiModel.isLoading)
+    }
+
+    @Test
+    fun `Load data failed`() = runBlocking {
+        // Given
+        whenever(catalogueRepository.getCategories()).thenThrow(IllegalStateException("Drill"))
+
+        // When
+        async { viewModel.onLoad() }.await()
+
+        // Then
+        val observedValues = testObserver.observedValues
+        Assert.assertEquals( 3, observedValues.size)
+        val onLoadingUiModel = observedValues[0]!!
+        Assert.assertTrue(onLoadingUiModel.isLoading)
+
+        val dataUiModel = observedValues[1]!!
+        Assert.assertEquals(null, dataUiModel.categories)
+        Assert.assertEquals(true, dataUiModel.hasError)
 
         val onCompletedUiModel = observedValues[2]!!
         Assert.assertFalse(onCompletedUiModel.isLoading)
